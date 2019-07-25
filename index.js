fb316ab440f5e74b1652a1613f989de82c1d3053
Job Zea <yoshua149@gmail.com>
2019-07-24 22:42:16 -0500

    Editando el index para validar en Heroku

--- index.js
+++ index.js
@@ -95,5 +95,5 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 app.listen(port, host, () => {
-    console.log('El servidor está funcionando correctamente');
+    console.log('El servidor está LISTO!!');
 }); 
